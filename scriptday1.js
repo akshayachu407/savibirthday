475f68af456217e5bcca5f58c65692fc0404e07e
Akshay <33223478+akshayachu407@users.noreply.github.com>
2022-06-21 21:54:41 -0700

    first data

---
+++ scriptday1.js
@@ -0,0 +1,11 @@
+MyHead();
+function MyHead(){
+    document.getElementById("head").innerHTML="hello" ;
+    document.getElementById("red").style.backgroundColor="red";
+    document.getElementById("head").style.fontSize="100px";
+}
+function MyHead1(){
+    document.getElementById("head").innerHTML="hello world" ;
+    document.getElementById("red").style.backgroundColor="blue";
+    document.getElementById("head").style.fontSize="100px";
+}
