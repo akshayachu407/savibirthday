33baa4067622317dfa7c47377d09d0c8420882d1
akshayachu407 <akshay.g.b.e@gmail.com>
2022-06-30 05:41:42 -0700

    added all changes

--- githtml/piccss/scripttextpop.js
+++ githtml/piccss/scripttextpop.js
@@ -1,84 +1,84 @@
-const elts = {
-    text1: document.getElementById("text1"),
-    text2: document.getElementById("text2")
-};
+// const elts = {
+//     text1: document.getElementById("text1"),
+//     text2: document.getElementById("text2")
+// };
 
-const texts = [
-    " Happy Birthday"," Sister",
-    "if i",
-    "Have",
-    "Money",
-    " i would",
-    "Hired a",
-    "plane ","& Wish u "
-];
+// const texts = [
+//     " Happy Birthday"," Sister",
+//     "if i",
+//     "Have",
+//     "Money",
+//     " i would",
+//     "Hired a",
+//     "plane ","& Wish u "
+// ];
 
-const morphTime = 1;
-const cooldownTime = 1;
+// const morphTime = 1;
+// const cooldownTime = 1;
 
-let textIndex = texts.length - 1;
-let time = new Date();
-let morph = 0;
-let cooldown = cooldownTime;
+// let textIndex = texts.length - 1;
+// let time = new Date();
+// let morph = 0;
+// let cooldown = cooldownTime;
 
-elts.text1.textContent = texts[textIndex % texts.length];
-elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+// elts.text1.textContent = texts[textIndex % texts.length];
+// elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 
-function doMorph() {
-    morph -= cooldown;
-    cooldown = 0;
+// function doMorph() {
+//     morph -= cooldown;
+//     cooldown = 0;
 
-    let fraction = morph / morphTime;
+//     let fraction = morph / morphTime;
 
-    if (fraction > 1) {
-        cooldown = cooldownTime;
-        fraction = 1;
-    }
+//     if (fraction > 1) {
+//         cooldown = cooldownTime;
+//         fraction = 1;
+//     }
 
-    setMorph(fraction);
-}
+//     setMorph(fraction);
+// }
 
-function setMorph(fraction) {
-    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+// function setMorph(fraction) {
+//     elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+//     elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
-    fraction = 1 - fraction;
-    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+//     fraction = 1 - fraction;
+//     elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+//     elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
-    elts.text1.textContent = texts[textIndex % texts.length];
-    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-}
+//     elts.text1.textContent = texts[textIndex % texts.length];
+//     elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+// }
 
-function doCooldown() {
-    morph = 0;
+// function doCooldown() {
+//     morph = 0;
 
-    elts.text2.style.filter = "";
-    elts.text2.style.opacity = "100%";
+//     elts.text2.style.filter = "";
+//     elts.text2.style.opacity = "100%";
 
-    elts.text1.style.filter = "";
-    elts.text1.style.opacity = "0%";
-}
+//     elts.text1.style.filter = "";
+//     elts.text1.style.opacity = "0%";
+// }
 
-function animate() {
-    requestAnimationFrame(animate);
+// function animate() {
+//     requestAnimationFrame(animate);
 
-    let newTime = new Date();
-    let shouldIncrementIndex = cooldown > 0;
-    let dt = (newTime - time) / 1000;
-    time = newTime;
+//     let newTime = new Date();
+//     let shouldIncrementIndex = cooldown > 0;
+//     let dt = (newTime - time) / 1000;
+//     time = newTime;
 
-    cooldown -= dt;
+//     cooldown -= dt;
 
-    if (cooldown <= 0) {
-        if (shouldIncrementIndex) {
-            textIndex++;
-        }
+//     if (cooldown <= 0) {
+//         if (shouldIncrementIndex) {
+//             textIndex++;
+//         }
 
-        doMorph();
-    } else {
-        doCooldown();
-    }
-}
+//         doMorph();
+//     } else {
+//         doCooldown();
+//     }
+// }
 
-animate();
+// animate();
